e752117fea7453975bd5b27e0248bd9dac6cc678
shafeeque <shafeeqsha06@gmail.com>
2025-10-27 11:47:41 +0530

    feat: add logging interceptor

---
+++ src/infrastructure/grpc/interceptors/logging.interceptor.ts
@@ -0,0 +1,37 @@
+import { LoggingService } from '@infrastructure/observability/logging/logging.service';
+import {
+  Injectable,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+
+@Injectable()
+export class LoggingInterceptor implements NestInterceptor {
+  constructor(private readonly logger: LoggingService) {}
+
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    const call$ = next.handle();
+    const methodName = context.getHandler().name;
+
+    this.logger.log(`gRPC method ${methodName} called`, {
+      ctx: 'LoggingInterceptor',
+    });
+
+    return call$.pipe(
+      tap({
+        next: () =>
+          this.logger.log(`gRPC method ${methodName} completed`, {
+            ctx: 'LoggingInterceptor',
+          }),
+        error: (error) =>
+          this.logger.error(
+            `gRPC method ${methodName} failed: ${error.message}`,
+            { error, ctx: 'LoggingInterceptor' },
+          ),
+      }),
+    );
+  }
+}
