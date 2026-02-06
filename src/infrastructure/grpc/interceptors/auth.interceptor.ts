93b5236100ae3aa0b9689fec4b5765c8243cd999
shafeeque <shafeeqsha06@gmail.com>
2026-02-06 18:42:40 +0530

    feat(interceptors): udpate grpc logging and tracing interceptors

--- src/infrastructure/grpc/interceptors/auth.interceptor.ts
+++ src/infrastructure/grpc/interceptors/auth.interceptor.ts
@@ -10,14 +10,13 @@ import { Observable } from 'rxjs';
 @Injectable()
 export class AuthInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-    const grpcMetadata = context.getArgByIndex(1); // Metadata is the second argument in gRPC
+    const grpcMetadata = context.getArgByIndex(1);
     const authToken = grpcMetadata.get('authorization')?.[0]?.toString();
 
     if (!authToken || !authToken.startsWith('Bearer ')) {
       throw new UnauthorizedException('Missing or invalid authorization token');
     }
 
-    // The token will be validated by JwtAuthGuard
     return next.handle();
   }
 }
