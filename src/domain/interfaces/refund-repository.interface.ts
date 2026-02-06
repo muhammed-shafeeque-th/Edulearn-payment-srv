9c2bfbd32c026541f2eb81d7dd84db135fec7905
shafeeque <shafeeqsha06@gmail.com>
2026-02-06 18:37:53 +0530

    refactor(ports): renamed src/application/interface to src/application/ports for better namingm

--- src/domain/interfaces/refund-repository.interface.ts
+++
@@ -1,8 +0,0 @@
-import { Refund } from '@domain/entities/refund';
-
-export abstract class RefundRepository {
-  abstract save(refund: Refund): Promise<void>;
-  abstract findById(id: string): Promise<Refund | null>;
-  abstract findByIdempotencyKey(idempotencyKey: string): Promise<Refund | null>;
-  abstract update(refund: Refund): Promise<void>;
-}
