54eb2ae15fbda4db7d0ab85734859cf84323a261
shafeeque <shafeeqsha06@gmail.com>
2026-04-05 13:31:29 +0530

    feat(exception): update PaymentFailedException and RefundFailedException types to extend from BaseException type

--- src/domain/exceptions/refund-failed.exception.ts
+++ src/domain/exceptions/refund-failed.exception.ts
@@ -1,6 +1,9 @@
-export class RefundFailedException extends Error {
+import { BaseException } from 'src/shared/exceptions/base-exception';
+import { ErrorCode } from 'src/shared/exceptions/error-codes';
+
+export class RefundFailedException extends BaseException {
   constructor(message: string) {
-    super(message);
+    super(ErrorCode.FAILED_PRECONDITION, message, 'REFUND_FAILURE_EXCEPTION');
     this.name = 'REFUND_FAILURE_EXCEPTION';
   }
 }
