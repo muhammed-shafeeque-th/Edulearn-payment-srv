8f070587158af436cb4e1257f7ebca9b505a81f0
shafeeque <shafeeqsha06@gmail.com>
2026-04-05 13:34:06 +0530

    feat(exception): update domain exception by extending from baseException type

--- src/domain/exceptions/domain.exceptions.ts
+++ src/domain/exceptions/domain.exceptions.ts
@@ -1,13 +1,26 @@
-import { status as GrpcStatus, ServiceError } from '@grpc/grpc-js';
-import { DomainException } from './base.exception';
+import { DomainException } from './domain.exception';
+import { ErrorCode } from 'src/shared/exceptions/error-codes';
 
 export class NotFoundException extends DomainException {
-  errorCode: string = 'NOT_FOUND_EXCEPTION';
   constructor(message?: string) {
-    super(message || `Resource your are requested not found`);
+    super(
+      ErrorCode.NOT_FOUND,
+      message || `Resource your are requested not found`,
+      'NOT_FOUND',
+    );
   }
-  serializeGrpcError(): ServiceError {
-    return this.toGrpcError(this.message, GrpcStatus.NOT_FOUND, this.errorCode);
+
+  serializeError(): { message: string; field?: string }[] {
+    return [{ message: this.message }];
+  }
+}
+export class PaymentNotFoundException extends DomainException {
+  constructor(message?: string) {
+    super(
+      ErrorCode.NOT_FOUND,
+      message || `Resource your are requested not found`,
+      'PAYMENT_NOT_FOUND',
+    );
   }
 
   serializeError(): { message: string; field?: string }[] {
@@ -17,11 +30,11 @@ export class NotFoundException extends DomainException {
 export class UserNotFoundException extends DomainException {
   errorCode: string = 'USER_NOT_FOUND_EXCEPTION';
   constructor(message?: string) {
-    super(message || `User your are requested not found`);
-  }
-
-  serializeGrpcError(): ServiceError {
-    return this.toGrpcError(this.message, GrpcStatus.NOT_FOUND, this.errorCode);
+    super(
+      ErrorCode.NOT_FOUND,
+      message || `User your are requested not found`,
+      'USER_NOT_FOUND',
+    );
   }
 
   serializeError(): { message: string; field?: string }[] {
@@ -29,16 +42,12 @@ export class UserNotFoundException extends DomainException {
   }
 }
 export class ClientServiceException extends DomainException {
-  errorCode: string = 'CLIENT_SERVICE_EXCEPTION';
+  errorCode: string = '';
   constructor(message?: string) {
-    super(message || `Something went wrong while client service request`);
-  }
-
-  serializeGrpcError(): ServiceError {
-    return this.toGrpcError(
-      this.message,
-      GrpcStatus.INVALID_ARGUMENT,
-      this.errorCode,
+    super(
+      ErrorCode.FAILED_PRECONDITION,
+      message || `Something went wrong while client service request`,
+      'CLIENT_SERVICE_EXCEPTION',
     );
   }
 
@@ -49,11 +58,11 @@ export class ClientServiceException extends DomainException {
 export class OrderNotFoundException extends DomainException {
   errorCode: string = 'ORDER_NOT_FOUND_EXCEPTION';
   constructor(message?: string) {
-    super(message || `Order your have requested not found`);
-  }
-
-  serializeGrpcError(): ServiceError {
-    return this.toGrpcError(this.message, GrpcStatus.NOT_FOUND, this.errorCode);
+    super(
+      ErrorCode.NOT_FOUND,
+      message || `Order your have requested not found`,
+      'ORDER_NOT_FOUND',
+    );
   }
 
   serializeError(): { message: string; field?: string }[] {
@@ -63,32 +72,10 @@ export class OrderNotFoundException extends DomainException {
 export class IdempotencyException extends DomainException {
   errorCode: string = 'IDEMPOTENCY_EXCEPTION';
   constructor(message?: string) {
-    super(message || `Idempotency exception`);
-  }
-
-  serializeGrpcError(): ServiceError {
-    return this.toGrpcError(
-      this.message,
-      GrpcStatus.ALREADY_EXISTS,
-      this.errorCode,
-    );
-  }
-
-  serializeError(): { message: string; field?: string }[] {
-    return [{ message: this.message }];
-  }
-}
-export class TimeoutException extends DomainException {
-  errorCode: string = 'TIMEOUT_EXCEPTION';
-  constructor(message?: string) {
-    super(message || `Timeout exception`);
-  }
-
-  serializeGrpcError(): ServiceError {
-    return this.toGrpcError(
-      this.message,
-      GrpcStatus.DEADLINE_EXCEEDED,
-      this.errorCode,
+    super(
+      ErrorCode.ALREADY_EXISTS,
+      message || `Idempotency exception`,
+      'IDEMPOTENCY_EXCEPTION',
     );
   }
 
@@ -99,14 +86,10 @@ export class TimeoutException extends DomainException {
 export class PaymentFailureException extends DomainException {
   errorCode: string = 'PAYMENT_FAILURE_EXCEPTION';
   constructor(message?: string) {
-    super(message || `Error while processing payment`);
-  }
-
-  serializeGrpcError(): ServiceError {
-    return this.toGrpcError(
-      this.message,
-      GrpcStatus.INVALID_ARGUMENT,
-      this.errorCode,
+    super(
+      ErrorCode.FAILED_PRECONDITION,
+      message || `Error while processing payment`,
+      'PAYMENT_FAILURE_EXCEPTION',
     );
   }
 
@@ -117,14 +100,10 @@ export class PaymentFailureException extends DomainException {
 export class RefundFailedException extends DomainException {
   errorCode: string = 'REFUND_FAILURE_EXCEPTION';
   constructor(message?: string) {
-    super(message || `Error while processing refund request`);
-  }
-
-  serializeGrpcError(): ServiceError {
-    return this.toGrpcError(
-      this.message,
-      GrpcStatus.INVALID_ARGUMENT,
-      this.errorCode,
+    super(
+      ErrorCode.FAILED_PRECONDITION,
+      message || `Error while processing refund request`,
+      'REFUND_FAILURE_EXCEPTION',
     );
   }
 
