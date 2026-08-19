8f6f9511b4b83bc5eca18fd5326815b71631752c
shafeeque <shafeeqsha06@gmail.com>
2026-08-19 10:33:02 +0530

    feat(redis): update redis service, module dependencies and depend on abstract services

--- src/infrastructure/redis/redis.module.ts
+++ src/infrastructure/redis/redis.module.ts
@@ -1,40 +1,23 @@
-import { AppConfigService } from '@infrastructure/config/config.service';
 import { Module } from '@nestjs/common';
-import { RedisModule as NestRedisModule } from '@liaoliaots/nestjs-redis';
 import { RedisClientImpl } from './redis.service';
-import { ICacheService } from '@application/adaptors/redis.interface';
-import { IEventProcessRepository } from '@domain/repositories/event-process-repository.interface';
 import { EventProcessRepositoryImpl } from './event-process.repository';
+import { AppConfigService } from '../config/config.service';
+import { ICacheService } from '@application/ports/redis.interface';
+import { IEventProcessRepository } from 'src/domain/repositories/event-process-repository.interface';
+import { CacheModule } from '@edulearn/nest';
 
 @Module({
   imports: [
-    NestRedisModule.forRootAsync({
+    CacheModule.forRootAsync({
       inject: [AppConfigService],
-      useFactory: (...args: unknown[]) => ({
-        config: [
-          {
-            name: 'default',
-            url: (args[0] as AppConfigService).redisUrl,
-            maxRetriesPerRequest: 3,
-            enableReadyCheck: true,
-            reconnectOnError: () => true,
-            enableAutoPipelining: true,
-            keyPrefix: (args[0] as AppConfigService).redisKeyPrefix,
-            connectTimeout: 5000,
-            keepAlive: 1000,
-            maxLoadingRetryTime: 10000,
-            retryStrategy: (times: number) => Math.min(times * 200, 2000),
-            poolOptions: {
-              min: (args[0] as AppConfigService).redisMinConnections,
-              max: (args[0] as AppConfigService).redisMaxConnections,
-            },
-          },
-          {
-            name: 'subscriber',
-            url: (args[0] as AppConfigService).redisUrl,
-            connectTimeout: 5000,
-          },
-        ],
+
+      useFactory: (config: AppConfigService) => ({
+        db: config.redisDb,
+        keyPrefix: config.redisKeyPrefix,
+        maxRetriesPerRequest: 5,
+        lazyConnect: true,
+        host: config.redisHost,
+        port: config.redisPort,
       }),
     }),
   ],
